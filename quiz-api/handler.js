0a5e5f259f8eeefd55e5613ac3bcf085e1018136
Jeremy Leng <jleng@bcap.com>
2017-11-30 17:14:40 -0800

    first commit

---
+++ quiz-api/handler.js
@@ -0,0 +1,193 @@
+'use strict';
+
+const uuid = require('uuid');
+const randomstring = require('randomstring');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  //console.log(event.body);
+  if (typeof event.body !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 
+        'Content-Type': 'text/plain',
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work 
+      },
+      body: 'Couldn\'t create the quiz. Request body is not string.',
+    });
+    return;
+  }
+  var newBody = event.body.replace(/""/g, 'null');
+  console.log(newBody);
+  
+  const timestamp = new Date().getTime();
+  const quiz = JSON.parse(newBody);
+  var idOk = false;
+  (function checkId() {
+    if (!idOk) {
+      quiz.id = randomstring.generate({
+        length:8,
+        charset:'alphanumeric',
+        capitalization:'lowercase'
+      });//uuid.v1();
+      console.log(quiz.id);    
+      dynamoDb.get({ TableName: process.env.DYNAMODB_TABLE, Key: { id: quiz.id } }, (error, data) => {
+        // handle potential errors
+        console.log(data);
+        if (error || !data.Item || !data.Item.id) {
+          idOk = true;
+        }
+        checkId();
+      });
+    }
+  }());
+
+  quiz.created = timestamp;
+  quiz.updated = timestamp;
+  
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: quiz
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 
+          'Content-Type': 'text/plain',
+          "Access-Control-Allow-Origin" : "*" // Required for CORS support to work 
+        },
+        body: 'Couldn\'t create the quiz.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      headers: { 
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work 
+      },
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
+
+module.exports.update = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  var newBody = event.body.replace(/""/g, 'null');
+  console.log(newBody);  
+
+  const quiz = JSON.parse(newBody);
+  const id = event.pathParameters.id
+  if (!quiz || !id) {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 
+        'Content-Type': 'text/plain',
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work 
+      },
+      body: 'Couldn\'t update the quiz.'
+    });
+    return;
+  }
+  
+  console.log(id);
+  quiz.updated = timestamp;  
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: id
+    },
+    Item: quiz
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 
+          'Content-Type': 'text/plain',
+          "Access-Control-Allow-Origin" : "*" // Required for CORS support to work 
+        },
+        body: 'Couldn\'t update the quiz item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      headers: { 
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work 
+      },
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
+
+module.exports.get = (event, context, callback) => {
+  const id = event.pathParameters.id
+  if (!id) {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 
+        'Content-Type': 'text/plain',
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work         
+      },
+      body: 'Couldn\'t get the quiz because id is missing.',
+    });
+    return;
+  }
+  console.log(id);
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: id
+    }
+  };
+
+  // delete the todo from the database
+  dynamoDb.get(params, (error, data) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 
+          'Content-Type': 'text/plain',
+          "Access-Control-Allow-Origin" : "*" // Required for CORS support to work           
+        },
+        body: 'Couldn\'t remove the quiz item: '+error,
+      });
+      return;
+    }
+    else {
+      const response = {
+        statusCode: 200,
+        headers: { 
+          "Content-Type": "application/json; charset=utf-8",
+          "Access-Control-Allow-Origin" : "*" // Required for CORS support to work 
+        },
+        body: JSON.stringify(data.Item),
+      };
+      callback(null, response);        
+    }
+  });
+};
---
+++ test-api/handler.js
@@ -0,0 +1,51 @@
+'use strict';
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.hello = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const item = event.queryStringParameters.item
+  if (!item) {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the todo item.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: 'TestTable',
+    Item: {
+      id: uuid.v1(),
+      text: item,
+      checked: false,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the todo item.' + error,
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
